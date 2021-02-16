57d74eff9d165113df17a21a7c56f365b28eb29e
skyvltu <skyvltu@naver.com>
2021-02-16 19:41:12 +0900

    2021/02/16 ch02 2_4_string 예제 완료

--- ch02/2_4_string.py
+++ ch02/2_4_string.py
@@ -25,5 +25,29 @@ print('msg의 7번째 문자 :', msg[6])
 print('msg의 ?번째 문자 :', msg[-1])
 
 # 문자열 슬라이스
+str = 'Hello Korea'
+
+print('str[0:6] :', str[0:6])
+print('str[6:11] :', str[6:11])
+print('str[:5] :', str[:5])
+print('str[6:] :', str[6:])
+
 # 문자열 분리
-# 문자열 포멧
+people = '김유신|김춘추|장보고|강감찬|이순신'
+
+p1, p2, p3, p4, p5 = people.split('|')
+
+print('p1 :', p1)
+print('p2 :', p2)
+print('p3 :', p3)
+print('p4 :', p4)
+print('p5 :', p5)
+
+# 문자열 포맷
+fstr1 = '%d월 %d일'
+fstr2 = '%d월 %d일 %s요일'
+
+print(fstr1 % (2, 16))
+print(fstr2 % (2, 16, '화'))
+
+
